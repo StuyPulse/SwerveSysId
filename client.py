ccb7e73e5a4a61744367f811f3ac2cc0bed883e0
BenG49 <64862590+BenG49@users.noreply.github.com>
2022-11-19 23:01:16 -0500

    Add json formatting and json config file

--- client.py
+++ client.py
@@ -1,36 +1,42 @@
 from networktables import NetworkTables
+from math import pi
 import json
 import sys
-
-NetworkTables.initialize(server='127.0.0.1')
 
 tests = ['fast-backward', 'fast-forward', 'slow-backward', 'slow-forward']
 
 if __name__ == '__main__':
+    data = {}
+    try:
+        with open('client.json') as f:
+            data = json.load(f)
+    except:
+        exit('Error: must create "client.json" file in same directory, see README.md for content specifications')
+
+    ### Networktables ###
+
+    NetworkTables.initialize(server=data['ip'])
+
     sd = NetworkTables.getTable('SmartDashboard')
 
     while not NetworkTables.isConnected():
         ...
 
-    first_test = True
-    with open(sys.argv[1], 'w') as f:
-        f.write('{')
+    json_str = '{'
 
-        for test in tests:
-            s = sd.getString(test, None)
-            if s is None:
-                continue
+    for test in tests:
+        s = sd.getString(test, None)
+        if s is None:
+            continue
 
-            s = s.replace('E', 'e')
-            s = s.replace('[', '[\n')
-            s = s.replace(']', '\n]')
-            s = s.replace(',', ',\n')
+        s = s.replace('E', 'e')
 
-            if not first_test:
-                f.write(',')
-            else:
-                first_test = False
-            
-            f.write(f'"{test}": [{s}]')
+        if len(json_str) > 1:
+            json_str += ','
         
-        f.write(', "sysid": true,"test": "Drivetrain", "units": "Meters", "unitsPerRotation": 0.319185814}')
+        json_str += f'"{test}": [{s}]'
+    
+    json_str += f',"sysid":true,"test":"Drivetrain","units":"Meters","unitsPerRotation":{data["wheelCircumference"] * pi}}}'
+    
+    with open(data['outputFile'], 'w') as f:
+        json.dump(json.loads(json_str), f, indent=4)
